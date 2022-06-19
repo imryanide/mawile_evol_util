151366ebe39f8afd72e5d731924a8e7e89f912a4
imryanide <rayanrafisheikh@gmail.com>
2022-06-19 21:13:30 +0530

    base code for getting mon, level and next evol

---
+++ test.py
@@ -0,0 +1,21 @@
+import re
+
+f = open("test.txt")
+
+print("file opened")
+
+for line in f:
+    line = line.lstrip()
+    evo = re.search(r"^\[SPECIES_*[A-Z]*", line)
+    if evo:
+        # Pokemon Name
+        mon = re.findall(r"^\[SPECIES_*[A-Z]*", line)[0][9:]
+        # Pokemon level
+        lvl = re.findall(r"[0-9][0-9]", evo.string)
+        if lvl:
+            lvl = lvl[0]
+        else:
+            lvl = "Final Evolution"
+        # Pokemon Next Evolution
+        next = re.findall(r"\, SPECIES.*\}\,$", evo.string)[0][10:-3]
+        print(f"{mon} -> {next} @ {lvl}")
