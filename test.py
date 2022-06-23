a7bf153dad5c43539768dc87c41cf7018a556f71
arti <arti@pop-os.localdomain>
2022-06-23 12:37:14 +0530

    Committer: arti <arti@pop-os.localdomain> Changes to be committed: 	new file:   output.txt 	modified:   test.py 	modified:   test.txt Insert Clauses to better single out mon evolution and get megas from source file

--- test.py
+++ test.py
@@ -1,22 +1,40 @@
 import re
 
-f = open("test2.txt")
+f = open("evolution.h")
 
 print("file opened")
-
+f2 = open("output.txt", "w")
 for line in f:
     line = line.lstrip()
     evo = re.search(r"^\[SPECIES_*[A-Z]*", line)
     if evo:
         # Pokemon Name
-        mon = re.findall(r"^\[SPECIES_*[A-Z]*", line)[0][9:]
+        mon = re.findall(r"^\[SPECIES_*.*\]", line)[0][9:-1]
         # Pokemon level
-        lvl = re.findall(r"[0-9][0-9]", evo.string)
+        lvl = re.findall(r"([0-9][0-9]|[0-9])", evo.string)
         if lvl:
             lvl = lvl[0]
         else:
             lvl = "Final Evolution"
         # Pokemon Next Evolution
-        next = re.findall(r"\, SPECIES.*\}\,$", evo.string)[0][10:-3]
-
-        print(f"{mon.capitalize()} -> {next.capitalize()} @ {lvl}")
+        megas = re.findall(r"SPECIES_MEGA_[A-z]*", evo.string)
+        if megas:
+            s = f"{mon.capitalize()} ->  "
+            for i in megas:
+                megaevo = i.split("_")[1:]
+                mgs = " ".join(map(str, megaevo))
+                s = s + mgs + " "
+            print(f"{s} via Stone")
+            f2.write(f"{s} via Stone \n")
+        else:
+            next = re.findall(r"\, SPECIES_.[A-z]*", evo.string)
+            if next:
+                nexts = f"{mon.capitalize()} -> "
+                for i in next:
+                    nextev = i.split("_")[1:]
+                    allevs = " ".join(map(str, nextev))
+                    nexts = nexts + allevs + " "
+                print(f"{nexts} @ {lvl}\n")
+                f2.write(f"{nexts} @ {lvl}\n")
+            # print(f"{mon.capitalize()} -> {next} @ {lvl}")
+            # f2.write(f"{mon.capitalize()} -> {next} @ {lvl} \n")
